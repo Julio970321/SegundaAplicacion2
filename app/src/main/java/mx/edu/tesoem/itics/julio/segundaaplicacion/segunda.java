864ea847b6d7cbe5a2b90631a548bebbb161ed03
funky97 <48741349+funky97@users.noreply.github.com>
2019-03-28 09:55:53 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/julio/segundaaplicacion/segunda.java
@@ -0,0 +1,13 @@
+package mx.edu.tesoem.itics.julio.segundaaplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class segunda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
